c071bd55a365e34f20ea53404dffc29aa67a030e
medidew <mweekes51@gmail.com>
2025-09-27 18:00:43 +0100

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/medidew/NadeGrabber
+
+go 1.24.0
+
+toolchain go1.24.7
+
+require (
+	github.com/golang/geo v0.0.0-20250917161122-64cb148137c6 // indirect
+	github.com/golang/snappy v1.0.0 // indirect
+	github.com/markus-wa/demoinfocs-golang/v5 v5.0.2 // indirect
+	github.com/markus-wa/go-unassert v0.1.3 // indirect
+	github.com/markus-wa/gobitread v0.2.5-0.20241202000432-3c3e0bc797c6 // indirect
+	github.com/markus-wa/godispatch v1.4.1 // indirect
+	github.com/markus-wa/quickhull-go/v2 v2.2.0 // indirect
+	github.com/oklog/ulid/v2 v2.1.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/exp v0.0.0-20250911091902-df9299821621 // indirect
+	google.golang.org/protobuf v1.36.9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/markus-wa/demoinfocs-golang/v5/pkg/demoinfocs"
+	"github.com/markus-wa/demoinfocs-golang/v5/pkg/demoinfocs/common"
+	ev "github.com/markus-wa/demoinfocs-golang/v5/pkg/demoinfocs/events"
+)
+
+type NadeLineup struct {
+	x           float64
+	y           float64
+	z           float64
+	view_x      float32
+	view_y      float32
+	isCrouching bool
+	isWalking   bool
+	grenadeType common.EquipmentType
+	username    string
+}
+
+func (nl NadeLineup) String() string {
+	descriptor := "<" + nl.username + "> threw a <" + nl.grenadeType.String() + ">"
+
+	if nl.isCrouching {
+		descriptor += " while crouching"
+	} else if nl.isWalking {
+		descriptor += " while walking"
+	}
+
+	return descriptor + "\n" +
+		"	setpos " +
+		strconv.FormatFloat(nl.x, 'f', -1, 64) + " " +
+		strconv.FormatFloat(nl.y, 'f', -1, 64) + " " +
+		strconv.FormatFloat(nl.z, 'f', -1, 64) + "; " +
+		"setang " +
+		strconv.FormatFloat(float64(nl.view_y), 'f', -1, 32) + " " +
+		strconv.FormatFloat(float64(nl.view_x), 'f', -1, 32) + "\n"
+}
+
+func main() {
+	demo, err := os.Open("demos/1-b23949fe-fade-4f61-b12d-f30640c4df99-1-1.dem")
+
+	if err != nil {
+		fmt.Printf("Error occurred opening demo file: %v", err)
+	}
+
+	smokes := []NadeLineup{}
+	flashes := []NadeLineup{}
+	nades := []NadeLineup{}
+	molotovs := []NadeLineup{}
+
+	p := demoinfocs.NewParser(demo)
+	p.RegisterEventHandler(func(event ev.GrenadeProjectileThrow) {
+		thrower := event.Projectile.Thrower
+		pos := thrower.Position()
+
+		nl := NadeLineup{
+			x:           pos.X,
+			y:           pos.Y,
+			z:           pos.Z,
+			view_x:      thrower.ViewDirectionX(),
+			view_y:      thrower.ViewDirectionY(),
+			isCrouching: thrower.IsDucking(),
+			isWalking:   thrower.IsWalking(),
+			grenadeType: event.Projectile.WeaponInstance.Type,
+			username:    thrower.Name,
+		}
+
+		switch event.Projectile.WeaponInstance.Type {
+		case common.EqMolotov:
+			molotovs = append(molotovs, nl)
+		case common.EqIncendiary:
+			molotovs = append(molotovs, nl)
+		case common.EqFlash:
+			flashes = append(flashes, nl)
+		case common.EqSmoke:
+			smokes = append(smokes, nl)
+		case common.EqHE:
+			nades = append(nades, nl)
+		}
+	})
+
+	p.ParseToEnd()
+
+	output_file, err := os.Create("lineups")
+
+	if err != nil {
+		fmt.Printf("Error creating output file: %v", err)
+	}
+
+	for _, smoke := range smokes {
+		output_file.WriteString(smoke.String())
+	}
+}
